c709acb54949e7dc40fd9faf9fb147ba0c95ea11
Carro De Carreras Cardenas Medina <bcv10g@hotmail.com>
2021-06-16 00:08:30 -0500

    Initial one

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduzca su presupuesto quincenal: ");
+        double quincena = Double.parseDouble(scanner.nextLine());
+        System.out.println("Su presupuesto es de: $" + quincena);
+
+        System.out.println("Introduzca el número de elementos a restar de su quincena: ");
+        int veces = Integer.parseInt(scanner.nextLine());
+
+        double gasto;
+        double residuo = quincena;
+
+        for(int i = 0; i < veces; i++) {
+            System.out.println("Introduzca su gasto no. " + (i + 1) +": ");
+            gasto = Double.parseDouble(scanner.nextLine());
+            residuo = residuo - gasto;
+        }
+        if(residuo >= 0) {
+            System.out.println("Lo que queda de su quincena es: $" + (residuo));
+        }else{
+            System.out.println("Le quedas debiendo: $" + (residuo * -1) + " al señor de la tienda.");
+        }
+    }
+}
